7538fea2187fc6e5b4c4e8cdde1ed48d15dda086
Manish <101639758+MaddYManI@users.noreply.github.com>
2024-08-25 16:28:34 +0530

    sas

--- src/Nav/Login.js
+++ src/Nav/Login.js
@@ -1,4 +1,15 @@
+import { S3Client, ListBucketsCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+
 function Login () {
-    return;
+    
+    const img_to_show= ()=>{
+        "dd"
+    }
+
+    return(
+        <div>
+            <img src={img_to_show} alt="images"/>
+        </div>
+    );
 }
 export default Login;
